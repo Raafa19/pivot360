17843ec8879228de1118991a7c393c0d68ed5691
Raafa19 <rafamsuarez19@gmail.com>
2025-10-04 19:51:35 -0300

    update 8

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://raafa19.github.io/pivot360/"
+  base: "/pivot360"
 })
